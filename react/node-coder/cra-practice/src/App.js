ad4123da91081bd4bd5610fb61768ed1749b3e8c
Dongryul KIM <doctor.k1994@gmail.com>
2023-03-01 02:11:42 +0900

    useEffect return 함수 학습

--- react/node-coder/cra-practice/src/App.js
+++ react/node-coder/cra-practice/src/App.js
@@ -2,33 +2,21 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value)
-  console.log("i run all the time");
-
+function Hello() {
   useEffect(() => {
-    if (keyword !== '' && keyword.length > 5){
-      console.log('Search for', keyword)
-    }
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("Call the API...");
+    console.log("created");
+    return () => console.log("destroyed");
   }, []);
+  return <h1>Hello!</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange}
-        type='text' 
-        placeholder="search here..." 
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
