8fb6d6463588583fe665fd89f6ff9e3ccd14c211
Dongryul KIM <doctor.k1994@gmail.com>
2023-03-01 01:42:16 +0900

    learned useEffect's dependencies

--- react/node-coder/cra-practice/src/App.js
+++ react/node-coder/cra-practice/src/App.js
@@ -4,8 +4,16 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value)
   console.log("i run all the time");
+
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5){
+      console.log('Search for', keyword)
+    }
+  }, [keyword]);
 
   useEffect(() => {
     console.log("Call the API...");
@@ -13,6 +21,12 @@ function App() {
 
   return (
     <div>
+      <input 
+        value={keyword}
+        onChange={onChange}
+        type='text' 
+        placeholder="search here..." 
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
