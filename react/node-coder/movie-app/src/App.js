74ec42260a816e8a5dcc487854efd89f6e4d2ca7
Dongryul KIM <doctor.k1994@gmail.com>
2023-03-04 23:54:27 +0900

    [chore] Movie Component로 분리

--- react/node-coder/movie-app/src/App.js
+++ react/node-coder/movie-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -21,19 +22,16 @@ function App() {
     <div>
       {loading ? (
         <h1>Loading...</h1>
-      ) : (
+      ) : ( 
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
+           <Movie 
+            key={movie.id}
+            coverImg={movie.medium_cover_image} 
+            title={movie.title} 
+            summary={movie.summary}
+            genres={movie.genres} 
+           />
           ))}
         </div>
       )}
