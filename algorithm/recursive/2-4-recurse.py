0ffb650a62c0c9426ee239dbc09082714335cc86
doctork4 <doctor.k1994@gmail.com>
2023-04-21 22:29:26 +0900

    재귀 2-4 문제풀이 완료

--- algorithm/recursive/2-4-recurse.py
+++ algorithm/recursive/2-4-recurse.py
@@ -1,6 +1,13 @@
 # 파라미터 some_list를 거꾸로 뒤집는 함수
 def flip(some_list):
-    # 여기에 코드를 작성하세요
+    
+    if len(some_list) > 1 :
+        last = some_list[len(some_list)-1]
+        some_list.pop()
+        return [ last ] + flip(some_list)
+    
+    if len(some_list) == 1 or len(some_list) == 0 :
+        return some_list
 
 # 테스트 코드
 some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
