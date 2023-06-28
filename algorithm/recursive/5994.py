0d1d345f43ee5e981f45a00c8ef8941eac899e56
doctork4 <doctor.k1994@gmail.com>
2023-06-28 23:41:45 +0900

    자릿수 합 재귀  풀이

--- algorithm/recursive/5994.py
+++ algorithm/recursive/5994.py
@@ -1,11 +1,15 @@
 def sum_digits(n):
-    string = str(n)
-    total = 0
-    for i in range (0, len(string)):
-        total += int(string[i])
-        i += 1
-    return total
-    
+    string_n = str(n)
+    # total = 0
+    # for i in range (0, len(string)):
+    #     total += int(string[i])
+    #     i += 1
+    # return total
+    # 재귀로 풀기
+    if len(string_n) == 1 :
+        return n
+    elif len(string_n) > 1 :
+        return sum_digits(int(string_n[:-1])) + sum_digits(int(string_n[-1]))
 
 # 테스트 코드
 print(sum_digits(22541))
