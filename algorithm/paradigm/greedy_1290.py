7de70ab04a9441ebd3d7648e66adac9a896d7178
doctork4 <doctor.k1994@gmail.com>
2023-07-13 13:51:14 +0900

    1290 효율 높인 풀이 추가

--- algorithm/paradigm/greedy_1290.py
+++ algorithm/paradigm/greedy_1290.py
@@ -5,10 +5,15 @@ def min_fee(pages_to_print):
     
     # 2. 개인별 벌금금액 더하기
     total = 0
-    for i in range(1, len(pages_to_print)+1):
-        for j in range(0, i):
-            total += pages_to_print[j]    
-        
+
+    # 처음 작성했던 풀이 => 반복문 중첩으로 효율 떨어짐
+    # for i in range(1, len(pages_to_print)+1):
+    #     for j in range(0, i):
+    #         total += pages_to_print[j]    
+    
+    # 곱셈을 활용한 합계금액 구하기
+    for i in range(len(pages_to_print)):
+        total += pages_to_print[i] * (len(pages_to_print)-i)
     return total
 
 
