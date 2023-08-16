5abdaf42bb6d3ca44f845c2fe3d6555f3591612e
doctork4 <doctor.k1994@gmail.com>
2023-08-16 20:50:25 +0900

    다크/화이트모드 설정 선택기능 및 스타일 추가

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "../styles/globals.css";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Header />
+      <Container page>
+        <Component {...pageProps} />
+      </Container>
+    </ThemeProvider>
+  );
 }
