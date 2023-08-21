69f8d812d0db09bbbd6b1896ab03d5b4aa7aa273
doctork4 <doctor.k1994@gmail.com>
2023-08-21 23:12:21 +0900

    Next 폰트 적용

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -3,11 +3,22 @@ import "../styles/globals.css";
 import Header from "@/components/Header";
 import Container from "@/components/Container";
 import Head from "next/head";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weights: ["400", "700"],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <style>{`
+          html {
+            font-family: ${notoSansKR.style.fontFamily};
+          }
+        `}</style>
         <title>WATCHIT</title>
       </Head>
       <ThemeProvider>
