81f3c2650d342cfd705916ab438782a05c10aa1c
doctork4 <doctor.k1994@gmail.com>
2023-08-26 22:26:46 +0900

    알고리즘 연습 1140 풀이

---
+++ algorithm/pract/1140.py
@@ -0,0 +1,23 @@
+def sublist_max(profits):
+    max_profit = 0
+    for i in (0, len(profits)):
+        total = 0
+        for j in (0, len(profits)):
+            for item in profits[i:j]:
+                total += item
+                if total > max_profit:
+                    max_profit = total
+    # 
+    #     max_profit = profits[0]
+    # for i in range(0, len(profits)):
+    #     total = 0
+    #     for j in range(i, len(profits)):
+    #         total += profits[j]
+    #         max_profit = max(total, max_profit)
+    return max_profit
+
+
+# 테스트 코드
+print(sublist_max([4, 3, 8, -2, -5, -3, -5, -3]))
+print(sublist_max([2, 3, 1, -1, -2, 5, -1, -1]))
+print(sublist_max([7, -3, 14, -8, -5, 6, 8, -5, -4, 10, -1, 8]))
