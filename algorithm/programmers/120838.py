f6172fbeda521d822552b899d5646e39fb684fc6
doctork4 <doctor.k1994@gmail.com>
2023-09-26 23:36:18 +0900

    프로그래머스 120838 풀이 완료

---
+++ algorithm/programmers/120838.py
@@ -0,0 +1,12 @@
+def solution(letter):
+    letter_lst = list(letter.split())
+    answer = ''
+    morse = {'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'}
+    for string in letter_lst:
+        answer += morse[string]
+    
+    return answer
