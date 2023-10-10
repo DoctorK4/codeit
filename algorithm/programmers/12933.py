bf0b6088da2f86fab928703df458eb35cc793e69
doctork4 <doctor.k1994@gmail.com>
2023-10-10 23:07:29 +0900

    프로그래머스 12933 답안 수정

--- algorithm/programmers/12933.py
+++ algorithm/programmers/12933.py
@@ -3,6 +3,11 @@ def solution(n):
     arr = []
     
     for num in n:
-        arr.append(num)
+        arr.append(int(num))
         
     arr.sort(reverse=True)
+    answer = ""
+    for num in arr:
+        answer += str(num)
+    
+    return int(answer)
